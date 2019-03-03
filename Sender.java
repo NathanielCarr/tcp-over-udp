7f1646b6761c3afbec34295175018588c591eed7
Nathaniel Carr <carr0170@mylaurier.ca>
2019-03-02 19:20:40 -0500

    Made Header class (can convert to-from single byte).

--- Sender.java
+++ Sender.java
@@ -17,20 +17,180 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+
 class Sender {
 
-    public static class RDTFileTransfer {
+    public static class SenderView {
+        private JFrame frmRdtSender;
+        private JTextField txtAddr;
+        private JTextField txtFile;
 
-        final static int HEADER_SIZE = 1;
+        /**
+         * Create the application.
+         */
+        public SenderView() {
+            initialize();
+        }
 
-        public static void fileSend(InetAddress addr, int port, int timeoutMicro, int mds, File file)
+        /**
+         * Initialize the contents of the frame.
+         */
+        private void initialize() {
+            frmRdtSender = new JFrame();
+            frmRdtSender.setTitle("RDT Sender");
+            frmRdtSender.setBounds(100, 100, 384, 238);
+            frmRdtSender.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frmRdtSender.getContentPane().setLayout(null);
+
+            JLabel lblAddr = new JLabel("Receiver IP address:");
+            lblAddr.setHorizontalAlignment(SwingConstants.LEFT);
+            lblAddr.setBounds(10, 11, 152, 14);
+            frmRdtSender.getContentPane().add(lblAddr);
+
+            txtAddr = new JTextField();
+            txtAddr.setHorizontalAlignment(SwingConstants.RIGHT);
+            txtAddr.setBounds(10, 27, 152, 20);
+            frmRdtSender.getContentPane().add(txtAddr);
+            txtAddr.setColumns(10);
+
+            JLabel lblColon = new JLabel(":");
+            lblColon.setHorizontalAlignment(SwingConstants.CENTER);
+            lblColon.setBounds(162, 30, 13, 14);
+            frmRdtSender.getContentPane().add(lblColon);
+
+            JLabel lblPort = new JLabel("Port number:");
+            lblPort.setHorizontalAlignment(SwingConstants.LEFT);
+            lblPort.setBounds(173, 11, 81, 14);
+            frmRdtSender.getContentPane().add(lblPort);
+
+            JLabel lblMyPort = new JLabel("My port number:");
+            lblMyPort.setHorizontalAlignment(SwingConstants.LEFT);
+            lblMyPort.setBounds(270, 11, 88, 14);
+            frmRdtSender.getContentPane().add(lblMyPort);
+
+            JLabel lblFile = new JLabel("File to send:");
+            lblFile.setHorizontalAlignment(SwingConstants.LEFT);
+            lblFile.setBounds(10, 58, 138, 14);
+            frmRdtSender.getContentPane().add(lblFile);
+
+            txtFile = new JTextField();
+            txtFile.setHorizontalAlignment(SwingConstants.RIGHT);
+            txtFile.setColumns(10);
+            txtFile.setBounds(10, 74, 348, 20);
+            frmRdtSender.getContentPane().add(txtFile);
+
+            JLabel lblMDS = new JLabel("Maximum datagram size (bytes):");
+            lblMDS.setHorizontalAlignment(SwingConstants.LEFT);
+            lblMDS.setBounds(10, 105, 162, 20);
+            frmRdtSender.getContentPane().add(lblMDS);
+
+            JLabel lblTimeout = new JLabel("Timeout (milliseconds):");
+            lblTimeout.setHorizontalAlignment(SwingConstants.LEFT);
+            lblTimeout.setBounds(10, 136, 162, 20);
+            frmRdtSender.getContentPane().add(lblTimeout);
+
+            JButton btnSend = new JButton("Send");
+            btnSend.setBounds(10, 165, 89, 23);
+            frmRdtSender.getContentPane().add(btnSend);
+
+            JLabel lblTransTime = new JLabel("");
+            lblTransTime.setHorizontalAlignment(SwingConstants.LEFT);
+            lblTransTime.setBounds(108, 167, 250, 20);
+            frmRdtSender.getContentPane().add(lblTransTime);
+
+            JSpinner spnMDS = new JSpinner();
+            spnMDS.setModel(new SpinnerNumberModel(new Integer(256), new Integer(1), null, new Integer(1)));
+            spnMDS.setBounds(172, 105, 186, 20);
+            frmRdtSender.getContentPane().add(spnMDS);
+
+            JSpinner spinner = new JSpinner();
+            spinner.setModel(new SpinnerNumberModel(new Integer(10000), new Integer(0), null, new Integer(1)));
+            spinner.setBounds(172, 136, 186, 20);
+            frmRdtSender.getContentPane().add(spinner);
+
+            JSpinner spnPort = new JSpinner();
+            spnPort.setBounds(172, 27, 88, 20);
+            frmRdtSender.getContentPane().add(spnPort);
+
+            JSpinner spnMyPort = new JSpinner();
+            spnMyPort.setBounds(270, 27, 88, 20);
+            frmRdtSender.getContentPane().add(spnMyPort);
+        }
+    }
+
+    public static class FileSender {
+
+        public static class Header {
+            private Boolean handshake;
+            private Boolean ack;
+            private Boolean fin;
+            private int seq;
+
+            public Header(boolean handshake, boolean ack, boolean fin, int seq) {
+                this.handshake = handshake;
+                this.ack = ack;
+                this.fin = fin;
+                this.seq = seq == 0 ? 0 : 1;
+            }
+
+            public Header(byte header) {
+                this.handshake = ((header >> 7) & 1) == 1;
+                this.ack = ((header >> 6) & 1) == 1;
+                this.fin = ((header >> 6) & 1) == 1;
+                this.seq = (header >> 6) & 1;
+            }
+
+            public Boolean getHandshakeBit() {
+                return this.handshake;
+            }
+
+            public Boolean getAckBit() {
+                return this.ack;
+            }
+
+            public Boolean getFinBit() {
+                return this.fin;
+            }
+
+            public Boolean getSeq() {
+                return this.seq;
+            }
+
+            public byte toByte() {
+                char[] bitArr = {
+                    this.handshake ? '1' : '0',
+                    this.ack ? '1' : '0',
+                    this.fin ? '1' : '0',
+                    this.seq != 0 ? '1' : '0',
+                    '0',
+                    '0',
+                    '0',
+                    '0'
+                };
+                return (byte) Byte.parseByte(String.valueOf(bitArr), 2);
+            }
+
+        }
+
+        public static void send(InetAddress addr, int port, int timeoutMicro, int mds, File file)
                 throws SocketException, IOException {
+
             int seq = 0;
             DatagramSocket socket = new DatagramSocket(port, addr);
+            socket.setSoTimeout((int) (timeoutMicro / 1000));
 
             handshake(socket);
 
-            socket.setSoTimeout((int) (timeoutMicro / 1000));
             Iterable<DatagramPacket> datagrams = makeDatagrams(file, mds, seq);
 
             try {
@@ -51,16 +211,9 @@ class Sender {
         }
 
         private static void handshake(DatagramSocket socket) {
-            // TODO handshake.
+            
         }
-
-        private static byte[] makeHeader(int isAck, int ackNum, int seqNum) {
-            byte[] header = new byte[HEADER_SIZE];
-            char[] bitArr = {isAck != 0 ? '1' : '0', ackNum != 0 ? '1' : '0', seqNum != 0 ? '1' : '0', '0', '0', '0', '0', '0' };
-            header[0] = (byte) Byte.parseByte(String.valueOf(bitArr), 2);
-            return header;
-        }
-
+        
         private static Iterable<DatagramPacket> makeDatagrams(File file, int mds, int startSeq) throws IOException {
             List<DatagramPacket> datagramList = new ArrayList<DatagramPacket>();
 
@@ -78,16 +231,12 @@ class Sender {
             return datagramList;
         }
 
-        private static String byteArrToString(byte[] bytes) {
-            return new String(bytes);
-        }
-
     }
 
     public static void main(String[] args) throws IOException {
         File file = new File("C:/Users/Nathaniel/Desktop/CP367 MT.txt");
         Iterable<DatagramPacket> datagrams = RDTFileTransfer.makeDatagrams(file, 5, 0);
-        
+
         List<Byte> bytes = new ArrayList<Byte>();
         for (DatagramPacket packet : datagrams) {
             byte[] pBytes = packet.getData();
